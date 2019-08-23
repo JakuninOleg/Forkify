529cd0309385908827ccab0cc233d3e7267a1cf3
Oleg Jakunin <oleg.kemz@gmail.com>
2019-08-24 01:03:35 +0300

    Results view and pagination

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,44 @@
 import Search from './models/Search';
+import * as searchView from './views/searchView';
+import { elements, renderLoader, clearLoader } from './views/base';
+import { basename } from 'path';
 
+// Global state of the app
+const state = {};
 
-const search = new Search('pizza');
+const controlSearch = async () => {
+  // 1. Get query from the view
+  const query = searchView.getInput();
 
+  if (query) {
+    // 2. Adding new object to the state
+    state.search = new Search(query);
 
-search.getResults();
+    // 3. Preparing UI for render
+    searchView.clearInput();
+    searchView.clearResults();
+    renderLoader(elements.searchRes);
+
+    // 4. Search for results
+    await state.search.getResults();
+
+    // 5. Redner result to the UI
+    clearLoader();
+    searchView.renderResults(state.search.result);
+  }
+}
+
+elements.searchForm.addEventListener('submit', e => {
+  e.preventDefault();
+  controlSearch();
+});
+
+elements.searchResPages.addEventListener('click', e => {
+  e.preventDefault;
+  const btn = e.target.closest('.btn-inline');
+  if(btn) {
+    const goToPage = parseInt(btn.dataset.goto, 10);
+    searchView.clearResults();
+    searchView.renderResults(state.search.result, goToPage);
+  }
+})
